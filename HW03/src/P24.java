bc55520465258afd6be4acb81ddb0871e4e01e08
moksaiyau <saiyau002@gmail.com>
2019-10-10 21:57:34 +0800

    first commit

---
+++ HW03/src/P24.java
@@ -0,0 +1,11 @@
+
+public class P24 {
+	public static void main(String[] args)
+	{
+		for (int i =1; i<=5;i++)
+		{
+			System.out.println("第"+i+"次的迥圈");
+		}
+		System.out.println("迥圈結束");
+	}
+}
---
+++ HW03/src/P26.java
@@ -0,0 +1,13 @@
+
+public class P26 {
+	public static void main(String[] args)
+	{
+		int i = 1;
+		while(i<=5)
+		{
+			System.out.println("第"+i+"次的迥圈");
+			i++;
+		}
+		System.out.println("迥圈結束");
+	}
+}
